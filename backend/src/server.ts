c7ba3653833c09a3dc73cab7cf054f03f843dd97
EmperorOwl <ray@raylin.dev>
2025-02-26 19:38:22 +1100

    update default backend port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,7 @@ import morgan from "morgan";
 dotenv.config();
 
 const app: Express = express();
-const port: string = process.env.PORT || "3000";
+const port: string = process.env.PORT || "8080";
 
 // Middleware
 app.use(express.json());
