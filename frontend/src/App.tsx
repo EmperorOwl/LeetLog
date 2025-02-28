10938e4fb99bdb1dbe683086b4c6fe412a5ceebd
EmperorOwl <ray@raylin.dev>
2025-02-28 16:11:10 +1100

    add basic login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+
+import Home from "./pages/Home.tsx";
+import Login from "./pages/Login.tsx";
+
 function App() {
   return (
     <>
-      <p>Hello World!</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
