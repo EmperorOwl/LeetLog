9931cacce2d02dbc10a6dd9f753f2eb27d3224d6
EmperorOwl <ray@raylin.dev>
2025-03-03 22:07:03 +1100

    drop users every time before creating admin

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,13 +2,13 @@ import dotenv from "dotenv";
 
 import app from "./app";
 import { connectDatabase } from "./db";
-import { checkAdmin } from "./auth";
+import { createAdmin } from "./auth";
 
 dotenv.config();
 
 const main = async () => {
   await connectDatabase();
-  await checkAdmin();
+  await createAdmin();
   const port: string | undefined = process.env.PORT;
   if (!port) {
     throw new Error("PORT is not defined");
