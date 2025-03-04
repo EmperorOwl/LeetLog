8d3d32da40f8c0240d017e5e73090f1168bd775b
EmperorOwl <ray@raylin.dev>
2025-03-04 21:01:27 +1100

    backend https

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,8 @@
 import dotenv from "dotenv";
+import http from "http";
+import https from "https";
+import fs from "fs";
+import path from "path";
 
 import app from "./app";
 import { connectDatabase } from "./db";
@@ -13,9 +17,25 @@ const main = async () => {
   if (!port) {
     throw new Error("PORT is not defined");
   }
-  app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  });
+
+  const isProd = process.env.NODE_ENV === "prod";
+
+  if (isProd) {
+    // Set up SSL certificate
+    const sslOptions = {
+      key: fs.readFileSync(path.resolve('/etc/letsencrypt/live/app.raylin.dev/privkey.pem')),
+      cert: fs.readFileSync(path.resolve('/etc/letsencrypt/live/app.raylin.dev/fullchain.pem'))
+    };
+    // Create HTTPS server
+    https.createServer(sslOptions, app).listen(port, () => {
+      console.log(`HTTPS Server running on port ${port}`);
+    });
+  } else {
+    // Create HTTP server
+    http.createServer(app).listen(port, () => {
+      console.log(`HTTP Server running on port ${port}`);
+    });
+  }
 };
 
 main();
